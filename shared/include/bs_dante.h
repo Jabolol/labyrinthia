4928093edab622524129e1f4ae9ac9d2cd0f5773
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-02 15:53:35 +0100

    [add](shared/include/bs_dante.h) Add `node_t` structure

--- shared/include/bs_dante.h
+++ shared/include/bs_dante.h
@@ -17,10 +17,24 @@
 #include <sys/wait.h>
 #include <termios.h>
 #include <unistd.h>
-#include "./oop/oop.h"
 
 #ifndef __DANTE_1
     #define __DANTE_1
+typedef struct node node_t;
+typedef struct node {
+    bool walkable;
+    struct {
+        int32_t x;
+        int32_t y;
+    } coords;
+    struct {
+        int32_t f;
+        int32_t g;
+        int32_t h;
+    } costs;
+    node_t *parent;
+} node_t;
 char **split(char *const readonly_str, bool (*fn)(char const c));
 void cleanup_split(char ***ptr);
+void free_class(void *ptr);
 #endif
