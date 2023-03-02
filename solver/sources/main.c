6097e1bbd4d2f2bcb897f8031f8140ffe0e1a757
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-02 15:55:01 +0100

    [add](solver/sources/main.c) Load `nodes`, testing

--- solver/sources/main.c
+++ solver/sources/main.c
@@ -5,7 +5,16 @@
 ** project entrypoint
 */
 
+#include "../include/bs_dante.h"
+#include "../include/oop/solver.h"
+
 int main(void)
 {
+    SolverClass *self __attribute__((cleanup(free_class))) = new_class(Solver);
+    self->load_file(self, "./maze.txt");
+    self->load_nodes(self);
+    self->tree->insert_leaf(self->tree, *self->data);
+    leaf_t *leaf = self->tree->get_smallest(self->tree->tree);
+    printf("-> %p\n", leaf);
     return 0;
 }
