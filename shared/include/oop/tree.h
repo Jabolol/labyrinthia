a1b7eaaf14428be8e74a345e3397740047b33225
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-07 19:09:10 +0100

    [add](shared/include/oop/tree.h) Rename `root`, add `utility` functions

--- shared/include/oop/tree.h
+++ shared/include/oop/tree.h
@@ -21,12 +21,14 @@ typedef struct tree_aux {
     /* Inheritance */
     class_t parent;
     /* Properties */
-    leaf_t *tree;
+    leaf_t *root;
+    size_t leaf_count;
     /* Methods */
     leaf_t *(*create_leaf)(node_t *);
     void (*insert_leaf)(TreeClass *, node_t *);
     leaf_t *(*get_smallest)(leaf_t *);
-    leaf_t *(*remove_leaf)(TreeClass *, leaf_t *, node_t *);
+    leaf_t *(*get_leaf)(TreeClass *self, node_t *value);
+    leaf_t *(*remove_leaf)(TreeClass *, node_t *);
 } TreeClass;
 extern const class_t *Tree;
 #endif
