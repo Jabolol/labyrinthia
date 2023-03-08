f7581a73244777b5833087c345c5b10e914505e2
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-08 17:55:51 +0100

    [add](solver/sources/main.c) Add `adjacent` and `get_adjacent` to `SolverClass`, minor tweaks

--- solver/sources/main.c
+++ solver/sources/main.c
@@ -19,6 +19,9 @@ int main(int argc, char **argv)
     self->load_file(self, *(argv + 1));
     self->load_nodes(self);
 
+    self->open = new_class(Tree, self->size - self->offset);
+    self->close = new_class(Tree, self->size - self->offset);
+
     node_t *target = *(self->data + self->size - self->offset - 1);
 
     self->open->insert_leaf(self->open, *self->data);
@@ -28,53 +31,54 @@ int main(int argc, char **argv)
         leaf_t *out = self->open->remove_leaf(self->open, smallest->node);
         self->close->insert_leaf(self->close, out->node);
 
-        if (memcmp(&smallest->node->coords, &target->coords, sizeof(target->coords)) == 0) {
+        if (memcmp(&smallest->node->coords, &target->coords,
+                sizeof(target->coords))
+            == 0) {
             node_t *curr = smallest->node;
             while (curr) {
                 *((char *) self->addr + (curr->coords.y * self->columns)
                     + curr->coords.x + curr->coords.y) = 'o';
                 curr = curr->parent;
             }
+            self->open->free(self->open, out);
             write(STDOUT_FILENO, (char *) self->addr, self->size);
             return 0;
         }
 
-        node_t **adjacent = self->get_adjacent(self, &smallest->node->coords);
+        self->get_adjacent(self, &smallest->node->coords);
 
         for (int8_t i = 0; i < 4; i++) {
-            if (!*(adjacent + i)) {
+            if (!*(self->adjacent + i)) {
                 continue;
             }
-            if (!(*(adjacent + i))->walkable
-                || self->close->get_leaf(self->close, (*(adjacent + i)))
+            if (!(*(self->adjacent + i))->walkable
+                || self->close->get_leaf(self->close, (*(self->adjacent + i)))
                     != NULL) {
                 continue;
             }
             double g = self->get_distance(
-                &(*(adjacent + i))->coords, &(*self->data)->coords);
-            double h = self->get_distance(&(*(adjacent + i))->coords, &target->coords);
+                &(*(self->adjacent + i))->coords, &(*self->data)->coords);
+            double h = self->get_distance(
+                &(*(self->adjacent + i))->coords, &target->coords);
             double f = g + h;
 
-            if (self->open->get_leaf(self->open, *(adjacent + i)) == NULL
-                || h < (*(adjacent + i))->costs.h) {
-                (*(adjacent + i))->parent = smallest->node;
-                (*(adjacent + i))->costs.f = f;
-                (*(adjacent + i))->costs.g = g;
-                (*(adjacent + i))->costs.h = h;
-                if (self->open->get_leaf(self->open, *(adjacent + i))
+            if (self->open->get_leaf(self->open, *(self->adjacent + i)) == NULL
+                || h < (*(self->adjacent + i))->costs.h) {
+                (*(self->adjacent + i))->parent = smallest->node;
+                (*(self->adjacent + i))->costs.f = f;
+                (*(self->adjacent + i))->costs.g = g;
+                (*(self->adjacent + i))->costs.h = h;
+                if (self->open->get_leaf(self->open, *(self->adjacent + i))
                     == NULL) {
-                    self->open->insert_leaf(self->open, *(adjacent + i));
+                    self->open->insert_leaf(self->open, *(self->adjacent + i));
                 }
             }
         }
 
-        free(adjacent);
-        free(out);
+        self->open->free(self->open, out);
     }
 
-    // TODO: free close nodes
-
-    printf("no solution found\n");
+    write(STDOUT_FILENO, "no solution found", 17);
 
     return 84;
 }
