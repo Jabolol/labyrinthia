4f3e395cf62d1c41ea117ef25abd09be8fae16ce
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-08 21:04:57 +0100

    [add](shared/include/oop/solver.h) Add `extra` utility function prototypes

--- shared/include/oop/solver.h
+++ shared/include/oop/solver.h
@@ -28,6 +28,8 @@ typedef struct solver_aux {
     size_t offset;
     size_t columns;
     node_t *pool;
+    node_t *target;
+    leaf_t *smallest;
     node_t **data;
     node_t **adjacent;
     TreeClass *open;
@@ -36,7 +38,16 @@ typedef struct solver_aux {
     void (*load_file)(SolverClass *, char *);
     void (*load_nodes)(SolverClass *);
     void (*get_adjacent)(SolverClass *, coords_t *);
-    double (*get_distance)(coords_t *, coords_t *);
+    void (*start_solver)(SolverClass *, char *);
+    void (*fill_nodes)(SolverClass *);
+    void (*prune_adjacent)(SolverClass *);
+    void (*handle_valid_adjacent)(SolverClass *, node_t *);
+    int (*print_maze)(SolverClass *, leaf_t *, leaf_t *);
 } SolverClass;
+void solver_start_solver(SolverClass *self, char *path);
+void solver_get_adjacent(SolverClass *self, coords_t *coords);
+void solver_prune_adjacent(SolverClass *self);
+void solver_handle_valid_adjacent(SolverClass *self, node_t *adjacent);
+int solver_print_maze(SolverClass *self, leaf_t *out, leaf_t *smallest);
 extern const class_t *Solver;
 #endif
