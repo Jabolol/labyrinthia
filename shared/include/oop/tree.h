e54f8dbab6d1015fc48dd207f415fe77f5eca9cc
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-08 17:53:06 +0100

    [add](shared/include/oop/tree.h) Implement own memory `allocator`

--- shared/include/oop/tree.h
+++ shared/include/oop/tree.h
@@ -23,12 +23,16 @@ typedef struct tree_aux {
     /* Properties */
     leaf_t *root;
     size_t leaf_count;
+    size_t max_leaves;
+    leaf_t *pool;
     /* Methods */
-    leaf_t *(*create_leaf)(node_t *);
+    leaf_t *(*create_leaf)(TreeClass *self, node_t *);
     void (*insert_leaf)(TreeClass *, node_t *);
     leaf_t *(*get_smallest)(leaf_t *);
     leaf_t *(*get_leaf)(TreeClass *self, node_t *value);
     leaf_t *(*remove_leaf)(TreeClass *, node_t *);
+    leaf_t *(*alloc)(TreeClass *);
+    void (*free)(TreeClass *, leaf_t *);
 } TreeClass;
 extern const class_t *Tree;
 #endif
