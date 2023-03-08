45072dd0ac58d9cf9db5f98f1b52dea0f1ef0fae
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-08 17:54:37 +0100

    [add](solver/sources/tree.c) Implement `alloc` and `free` on a memory pool for the `binary tree`

--- solver/sources/tree.c
+++ solver/sources/tree.c
@@ -7,10 +7,26 @@
 
 #include "../include/oop/tree.h"
 
-leaf_t *tree_create_leaf(node_t *node)
+void tree_constructor(void *ptr, va_list *args)
+{
+    TreeClass *self = (TreeClass *) ptr;
+    self->max_leaves = va_arg(*args, size_t);
+    if (!((self->pool = calloc(self->max_leaves, sizeof(leaf_t))))) {
+        fprintf(stderr, "Failed malloc allocation\n");
+        exit(84);
+    }
+}
+
+void tree_destructor(void *ptr)
+{
+    TreeClass *self = (TreeClass *) ptr;
+    free(self->pool);
+}
+
+leaf_t *tree_create_leaf(TreeClass *self, node_t *node)
 {
     leaf_t *leaf = NULL;
-    if (!((leaf = malloc(sizeof(*leaf))))) {
+    if (!((leaf = self->alloc(self)))) {
         return NULL;
     }
     return ({ leaf->left = leaf->right = NULL, leaf->node = node, leaf; });
@@ -22,8 +38,7 @@ void tree_insert_leaf(TreeClass *self, node_t *value)
     leaf_t *parent = NULL;
 
     if (self->root == NULL) {
-        self->root = self->create_leaf(value);
-        self->leaf_count += 1;
+        self->root = self->create_leaf(self, value);
         return;
     }
 
@@ -36,11 +51,10 @@ void tree_insert_leaf(TreeClass *self, node_t *value)
         }
     }
 
-    self->leaf_count += 1;
     if (value->costs.h < parent->node->costs.h) {
-        parent->left = self->create_leaf(value);
+        parent->left = self->create_leaf(self, value);
     } else {
-        parent->right = self->create_leaf(value);
+        parent->right = self->create_leaf(self, value);
     }
 }
 
@@ -91,8 +105,8 @@ leaf_t *tree_remove_leaf(TreeClass *self, node_t *value)
     leaf_t *parent = NULL;
     leaf_t *current = self->root;
     struct {
-        int left : 2;
-        int right : 2;
+        int32_t left : 2;
+        int32_t right : 2;
     } flag = {0, 0};
 
     while (current) {
@@ -104,7 +118,7 @@ leaf_t *tree_remove_leaf(TreeClass *self, node_t *value)
             flag.right = !((flag.left = 1));
         } else {
             current = current->right;
-            flag.left ^= flag.right ^= ((flag.right = flag.left));
+            flag.right = flag.left, flag.left ^= flag.right;
         }
     }
 
@@ -140,9 +154,12 @@ leaf_t *tree_remove_leaf(TreeClass *self, node_t *value)
             new_root = new_root->right;
         }
         new_root->right = right;
-        self->root == current ? (self->root = current->left)
-            : flag.left       ? (parent->left = current->left)
-                              : ((parent->right = current->left));
+        (self->root == current) ? (self->root = current->left) : ({
+            if (flag.left) {
+                parent->left = current->left;
+            } else
+                parent->right = current->left;
+        });
         self->leaf_count -= 1;
         return current;
     }
@@ -151,20 +168,45 @@ leaf_t *tree_remove_leaf(TreeClass *self, node_t *value)
     return current;
 }
 
+leaf_t *tree_alloc(TreeClass *self)
+{
+    if (self->leaf_count >= self->max_leaves) {
+        return NULL;
+    }
+    self->leaf_count += 1;
+    for (size_t i = 0; i < self->max_leaves; i++) {
+        if (((self->pool + i))->node == NULL) {
+            return (self->pool + i);
+        }
+    }
+    return NULL;
+}
+
+void tree_free(TreeClass *self, leaf_t *ptr)
+{
+    if (ptr < self->pool || ptr > self->pool + self->max_leaves) {
+        __builtin_unreachable();
+    }
+    memset(ptr, 0, sizeof(*ptr));
+}
+
 const TreeClass init_tree = {
     {
         ._size = sizeof(TreeClass),
         ._name = "Tree",
-        ._constructor = NULL,
-        ._destructor = NULL,
+        ._constructor = &tree_constructor,
+        ._destructor = &tree_destructor,
     },
     .root = NULL,
     .leaf_count = 0,
+    .max_leaves = 0,
     .create_leaf = &tree_create_leaf,
     .get_smallest = &tree_get_smallest,
     .insert_leaf = &tree_insert_leaf,
     .remove_leaf = &tree_remove_leaf,
     .get_leaf = &tree_get_leaf,
+    .alloc = &tree_alloc,
+    .free = &tree_free,
 };
 
 const class_t *Tree = (const class_t *) &init_tree;
