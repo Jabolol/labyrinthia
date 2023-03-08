2b9c05dcf6dcc8c7ffc60f8f560573dd8e2596c3
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-08 21:05:21 +0100

    [add](shared/include/oop/tree.h) Add `flag_t` declaration, `util` function prototypes

--- shared/include/oop/tree.h
+++ shared/include/oop/tree.h
@@ -16,6 +16,10 @@ typedef struct leaf {
     leaf_t *left;
     leaf_t *right;
 } leaf_t;
+typedef struct {
+    int32_t left : 2;
+    int32_t right : 2;
+} flag_t;
 typedef struct tree_aux TreeClass;
 typedef struct tree_aux {
     /* Inheritance */
@@ -32,7 +36,21 @@ typedef struct tree_aux {
     leaf_t *(*get_leaf)(TreeClass *self, node_t *value);
     leaf_t *(*remove_leaf)(TreeClass *, node_t *);
     leaf_t *(*alloc)(TreeClass *);
+    leaf_t *(*remove_no_child)(TreeClass *, leaf_t *, leaf_t *, flag_t *);
+    leaf_t *(*remove_one_child)(TreeClass *, leaf_t *, leaf_t *, flag_t *);
+    leaf_t *(*remove_two_children)(TreeClass *, leaf_t *, leaf_t *, flag_t *);
+    void (*traverse_tree)(leaf_t **, leaf_t **, node_t *, flag_t *);
     void (*free)(TreeClass *, leaf_t *);
 } TreeClass;
+
+void tree_traverse_tree(leaf_t **, leaf_t **, node_t *, flag_t *);
+leaf_t *tree_remove_no_child(TreeClass *, leaf_t *, leaf_t *, flag_t *);
+leaf_t *tree_remove_one_child(TreeClass *, leaf_t *, leaf_t *, flag_t *);
+leaf_t *tree_remove_two_children(TreeClass *, leaf_t *, leaf_t *, flag_t *);
+leaf_t *tree_remove_leaf(TreeClass *, node_t *);
+leaf_t *tree_create_leaf(TreeClass *, node_t *);
+void tree_insert_leaf(TreeClass *, node_t *);
+leaf_t *tree_get_leaf(TreeClass *, node_t *);
+leaf_t *tree_get_smallest(leaf_t *);
 extern const class_t *Tree;
 #endif
