db5b5609b0b24a353269307b9933ed4d2caa3706
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-09 10:56:24 +0100

    [add](solver/sources/solver.c) Move `core` functions, `initialize` class properties

--- solver/sources/solver.c
+++ solver/sources/solver.c
@@ -27,63 +27,6 @@ void solver_destructor(void *ptr)
     free(self->adjacent);
 }
 
-void solver_load_path(SolverClass *self, char *path)
-{
-    int32_t fd = open(path, O_RDONLY);
-    if (fd < 0) {
-        fprintf(stderr, "Couldn't open %s\n", path);
-        exit(EXIT_ERROR);
-    }
-    self->size = lseek(fd, 0, SEEK_END);
-    self->addr =
-        mmap(NULL, self->size, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
-    if (self->addr == MAP_FAILED) {
-        close(fd);
-        fprintf(stderr, "Failed mmap allocation\n");
-        exit(EXIT_ERROR);
-    }
-    close(fd);
-}
-
-void solver_fill_nodes(SolverClass *self)
-{
-    char *string = (char *) self->addr;
-    self->columns = (strchr(string, '\n') - string);
-    node_t *node = NULL;
-    int32_t offset = 0;
-
-    for (size_t i = 0; i < self->size; i++) {
-        if (*(string + i) == '\n') {
-            offset++;
-            continue;
-        }
-        *(self->data + i - offset) = self->pool + i - offset;
-        node = *(self->data + i - offset);
-        node->walkable = *(string + i) == '*';
-        node->costs.g = INT16_MAX;
-        node->costs.h = INT16_MAX;
-        node->costs.f = node->costs.f + node->costs.g;
-        node->coords.x = (i - offset) % self->columns;
-        node->coords.y = (i - offset) / self->columns;
-    }
-    self->offset = offset;
-}
-
-void solver_load_nodes(SolverClass *self)
-{
-    if (!((self->pool = calloc(self->size + 1, sizeof(node_t))))) {
-        fprintf(stderr, "Failed calloc allocation\n");
-        exit(EXIT_ERROR);
-    }
-    if (!((self->data = calloc(self->size + 1, sizeof(node_t *))))) {
-        fprintf(stderr, "Failed calloc allocation\n");
-        exit(EXIT_ERROR);
-    }
-    self->fill_nodes(self);
-    self->open = new_class(Tree, self->size - self->offset);
-    self->close = new_class(Tree, self->size - self->offset);
-}
-
 const SolverClass init_solver = {
     {
         ._size = sizeof(SolverClass),
@@ -100,6 +43,7 @@ const SolverClass init_solver = {
     .size = 0,
     .columns = 0,
     .offset = 0,
+    .error = false,
     .load_file = &solver_load_path,
     .load_nodes = &solver_load_nodes,
     .get_adjacent = &solver_get_adjacent,
@@ -108,6 +52,8 @@ const SolverClass init_solver = {
     .handle_valid_adjacent = &solver_handle_valid_adjacent,
     .print_maze = &solver_print_maze,
     .fill_nodes = &solver_fill_nodes,
+    .solve_maze = &solver_solve_maze,
+    .is_valid = &solver_is_valid,
 };
 
 const class_t *Solver = (const class_t *) &init_solver;
