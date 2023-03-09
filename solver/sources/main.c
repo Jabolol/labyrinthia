e4be62df8528c9bdf215b3f137de555348db3b22
Javier Ríos <javier.rios-urbano@epitech.eu>
2023-03-09 10:54:42 +0100

    [add](solver/sources/main.c) Cleanup `main`, implement `error` handling

--- solver/sources/main.c
+++ solver/sources/main.c
@@ -7,27 +7,15 @@
 
 #include "../include/oop/solver.h"
 
-// TODO(jabolo): Cleanup this code :)
-// TODO(jabolo): Error handling
 int main(int __attribute__((unused)) argc, char **argv)
 {
     SolverClass *self = new_class(Solver);
     self->start_solver(self, *(argv + 1));
-
-    while (self->open->leaf_count > 0) {
-        self->smallest = self->open->get_smallest(self->open->root);
-        leaf_t *out =
-            self->open->remove_leaf(self->open, self->smallest->node);
-        self->close->insert_leaf(self->close, out->node);
-        if (memcmp(&self->smallest->node->coords, &self->target->coords,
-                sizeof(self->target->coords))
-            == 0)
-            self->print_maze(self, out, self->smallest);
-        self->get_adjacent(self, &self->smallest->node->coords);
-        self->prune_adjacent(self);
-        self->open->free(self->open, out);
+    if (self->error || (((self->error = !self->is_valid(self))))) {
+        destroy_class(self);
+        return EXIT_ERROR;
     }
-    write(STDOUT_FILENO, "no solution found", 17);
+    self->solve_maze(self);
     destroy_class(self);
     return EXIT_ERROR;
 }
